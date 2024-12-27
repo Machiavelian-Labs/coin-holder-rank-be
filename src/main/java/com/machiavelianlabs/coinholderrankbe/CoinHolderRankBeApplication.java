1cb32c0cf1cbcee7ab6a21dc70f265b2ae3fca55
[4기-1반] 양권민 <didrnjsals@gmail.com>
2024-12-27 19:06:43 +0900

    feat: create init project

---
+++ src/main/java/com/machiavelianlabs/coinholderrankbe/CoinHolderRankBeApplication.java
@@ -0,0 +1,13 @@
+package com.machiavelianlabs.coinholderrankbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinHolderRankBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinHolderRankBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/machiavelianlabs/coinholderrankbe/CoinHolderRankBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machiavelianlabs.coinholderrankbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinHolderRankBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
